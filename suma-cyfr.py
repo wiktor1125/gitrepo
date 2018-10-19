ab0c084dd6fd0831354d5c29cd90ace411566b9e
wiktor1125 <wiktor1125@o2.pl>
2018-10-19 14:19:39 +0200

    tak właśnie

--- suma-cyfr.py
+++ suma-cyfr.py
@@ -1,28 +1,33 @@
+
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
 #  suma-cyfr.py
 #  
 
-
-def main(args):
-    
-    liczba = input("Podaj liczbę 2-cyfrową: ")
-    liczba = int(liczba)
-    
-    while liczba < 10:
-        liczba = input("Podaj liczbę 2-cyfrową: ")
-        liczba = int(liczba)
- 
+def suma1(liczba):
     suma = 0
     while liczba > 0:
         suma += liczba % 10
-        liczba = int(liczba/ 10)
-        
-    print("Suma:", suma)
+        liczba = int(liczba / 10)
+    return suma
     
+def suma2(liczba):
+    suma = 0
+    for cyfra in str(liczba):
+        suma += int(cyfra)
+    return suma
+    
+def main(args):
+    liczba = int(input("Podaj liczbę dwucyfrową: "))
+    
+    while liczba < 10:
+        liczba = int(input("Podaj liczbę dwucyfrową: "))
+    
+    print("Suma: ", suma2(liczba))
+		
     return 0
 
 if __name__ == '__main__':
     import sys
-    sys.exit(main(sys.argv))
+sys.exit(main(sys.argv))
---
+++ suma_cyfr.cpp
@@ -0,0 +1,43 @@
+/*
+ * suma_cyfr.cpp
+ */
+
+
+#include <iostream>
+
+using namespace std;
+
+int sumuj_cyfry1(int liczba){
+    
+    int suma = 0;
+    while(liczba > 0){
+        suma += liczba % 10;
+        liczba = liczba/ 10;
+        
+        
+        
+        }
+    
+        return suma;
+    
+    }
+
+
+
+
+
+int main(int argc, char **argv)
+{
+	int liczba = 0;
+    while (liczba < 10){
+        cout << "Podaj liczbę 2 - cyfrową: ";
+        cin >> liczba;
+        
+        }
+        
+        cout << "Suma : " << sumuj_cyfry1(liczba) << endl;
+    
+    
+	return 0;
+}
+

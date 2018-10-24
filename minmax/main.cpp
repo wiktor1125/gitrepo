3f46c42706f9b79e9a14835341738e984e2b9974
wiktor1125 <wiktor1125@o2.pl>
2018-10-24 10:35:04 +0200

    tak właśnie

---
+++ minmax/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+void minmax1(){
+    char dalej = 't';
+    int liczba = 0;
+    int min = 0;
+    while(dalej = 't'){
+        cout << "Podaj liczbę: ";
+    }
+
+}
+
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
---
+++ uczniowie/uczniowie.sql
@@ -0,0 +1,46 @@
+DROP TABLE IF EXISTS tbUczniowie;
+
+CREATE TABLE tbUczniowie( 
+id INTEGER PRIMARY KEY AUTOINCREMENT, 
+imie TEXT,
+nazwisko TEXT,
+plec BOOLEAN,
+id_klasa INTEGER,
+egz_hum NUMERIC NOT NULL DEFAULT 0,
+egz_mat NUMERIC NOT NULL DEFAULT 0,
+egz_jez NUMERIC NOT NULL DEFAULT 0,
+FOREIGN KEY (id_klasa) REFERENCES klasy(id)
+);
+
+DROP TABLE IF EXISTS tbKlasy;
+
+CREATE TABLE tbKlasy(
+id INTEGER,
+klasa TEXT,
+rok_naboru STRING,
+rok_matury STRING
+
+
+);
+
+DROP TABLE IF EXISTS tbPrzedmioty;
+
+CREATE TABLE tbPrzedmioty(
+id INTEGER,
+przedmiot TEXT,
+imie_naucz TEXT,
+nazwisko_naucz TEXT,
+plec_naucz BOOLEAN
+);
+
+DROP TABLE IF EXISTS tbOceny;
+
+CREATE TABLE tbOceny(
+id INTEGER NOT NULL,
+datad DATE,
+id_uczen INTEGER,
+id_przedmiot INTEGER,
+ocena INTEGER,
+FOREIGN KEY (id_uczen) REFERENCES uczniowie(id),
+FOREIGN KEY (id_przedmiot) REFERENCES przedmioty(id)
+);
